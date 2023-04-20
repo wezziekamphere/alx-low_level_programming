fab109c858a51ec1aff92574bb5d33bdecc8dd27
wezziekamphere <kampherewezzie@gmail.com>
2023-04-20 15:01:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/* main - c's entry point */
+/* main - entry point of c */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
